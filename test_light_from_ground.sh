ec5c0291141179f5fe7860cd23c8c86939b1876c
Bo Shang <cnpcshangbo@gmail.com>
2020-12-13 15:54:18 -0600

    Improved shell script robustness.

--- test_light_from_ground.sh
+++ test_light_from_ground.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Light from ground:
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+echo DIR = $DIR
 # python ~/sda/zed-opencv/python/zed-opencv.py /home/nvidia/sda/SVO/HD720_SN2353053_16-04-32.svo
-python ~/sda/zed-opencv/python/zed-opencv.py /home/nvidia/sda/SVO/output.svo
+python $DIR/python/zed-opencv.py /home/bo/SVO/output.svo
