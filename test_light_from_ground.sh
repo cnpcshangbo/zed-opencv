2e505e8172d23812625560b75bdec998757e0476
Bo Shang <cnpcshangbo@gmail.com>
2020-12-14 10:45:43 -0600

    updated zed-opencv.py parameter format

--- test_light_from_ground.sh
+++ test_light_from_ground.sh
@@ -3,4 +3,4 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 echo DIR = $DIR
 # python ~/sda/zed-opencv/python/zed-opencv.py /home/nvidia/sda/SVO/HD720_SN2353053_16-04-32.svo
-python $DIR/python/zed-opencv.py /home/bo/SVO/output.svo
+python $DIR/python/zed-opencv.py --source /home/bo/SVO/output.svo --ip 0.0.0.0 --port 8000
